0cb1e8717ce5c17fff906bc1f562de19a8ed2d7f
Rayyan Merchant <rayyan.merchant@gmail.com>
2022-12-06 10:03:46 -0500

    weekly scrum 10/26: creating new folder for experimental features in case we want to take a crack at problems others are having

---
+++ test/teamraad/src/index.js
@@ -0,0 +1,8 @@
+import {useMemo} from "react";
+import { GoogleMap, useLoadScript, Market } from "@react-google-maps/api";
+
+export default function Home() {
+const{} = useLoadScript
+return <div>Map[</div>
+
+}
