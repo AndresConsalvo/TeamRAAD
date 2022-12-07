255a3311e31f46fa67bc19cfab08f7b29fb2523c
notsinned <62024423+notsinned@users.noreply.github.com>
2022-12-06 22:54:12 -0500

    Update index.js
    Removed unused import of 'Market'

--- test/teamraad/src/index.js
+++ test/teamraad/src/index.js
@@ -1,5 +1,5 @@
 import {useMemo} from "react";
-import { useLoadScript, Market } from "@react-google-maps/api";
+import { useLoadScript } from "@react-google-maps/api";
 
 export default function Home() {
 const{} = useLoadScript
