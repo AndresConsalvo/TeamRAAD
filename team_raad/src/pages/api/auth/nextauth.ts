0b3cad13d0f77801bc6192c60438edad16825562
Rayyan Merchant <rayyan.merchant@gmail.com>
2022-12-06 21:52:40 -0500

    testing readme edit because git isnt letting any of us commit post transfer

---
+++ team_raad/src/pages/api/auth/nextauth.ts
@@ -0,0 +1,47 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log("AQUI", user)
+      return "/restricted"
+    }
+  },
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    DiscordProvider({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET
+    }),
+    // ...add more providers here
+  ],
+
+  pages: {
+    signIn: '/',
+    // signOut: '/auth/signout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+    // verifyRequest: '/auth/verify-request', // (used for check email message)
+    // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  }
+};
+
+export default NextAuth(authOptions);
